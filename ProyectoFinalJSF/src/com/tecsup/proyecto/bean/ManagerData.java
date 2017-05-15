6be491809280c8325a3303b1361ef42094b97f0e
Gustavoch25 <gustavo.chavez.c@tecsup.edu.pe>
2017-05-15 14:53:39 -0500

    prueba

---
+++ ProyectoFinalJSF/src/com/tecsup/proyecto/bean/ManagerData.java
@@ -0,0 +1,214 @@
+package com.tecsup.proyecto.bean;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import com.proyecto.myshop.bean.Producto;
+import com.proyecto.myshop.db.ConeccionDB;
+
+import oracle.sql.BLOB;
+
+@ManagedBean(name = "managerData", eager = true)
+@SessionScoped
+public class ManagerData implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String inNombre;
+	private String inMarca;
+	private String inDireccion;
+	private int inCalificacion;
+	private String inPrecio;
+	private BLOB inFoto;
+	
+	private Producto producto;
+
+	
+	public String getInNombre() {
+		return inNombre;
+	}
+
+	public void setInNombre(String inNombre) {
+		this.inNombre = inNombre;
+	}
+
+	public String getInMarca() {
+		return inMarca;
+	}
+
+	public void setInMarca(String inMarca) {
+		this.inMarca = inMarca;
+	}
+
+	public String getInDireccion() {
+		return inDireccion;
+	}
+
+	public void setInDireccion(String inDireccion) {
+		this.inDireccion = inDireccion;
+	}
+
+	public int getInCalificacion() {
+		return inCalificacion;
+	}
+
+	public void setInCalificacion(int inCalificacion) {
+		this.inCalificacion = inCalificacion;
+	}
+	
+	public String getInPrecio() {
+		return inPrecio;
+	}
+
+	public void setInPrecio(String inPrecio) {
+		this.inPrecio = inPrecio;
+	}
+
+	public BLOB getInFoto() {
+		return inFoto;
+	}
+
+	public void setInFoto(BLOB inFoto) {
+		this.inFoto = inFoto;
+	}
+
+	public Producto getProducto() {
+		return producto;
+	}
+
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
+
+	public ArrayList<Producto> getProductos() {
+		ResultSet rs = null;
+		PreparedStatement pst = null;
+		Connection con = ConeccionDB.getConnection();
+		String stm = "Select nombre, marca, direccion, calificacion, precio, foto from PRODUCTOS";
+		ArrayList<Producto> records = new ArrayList<Producto>();
+		//employees = new ArrayList<Employee>();
+		try {
+			pst = con.prepareStatement(stm);
+			pst.execute();
+			rs = pst.getResultSet();
+			while (rs.next()) {
+				Producto emp = new Producto();
+				emp.setNombre(rs.getString(1));
+				emp.setMarca(rs.getString(2));
+				emp.setDireccion(rs.getString(3));
+				emp.setCalificacion(rs.getInt(4));
+				emp.setPrecio(rs.getString(5));
+				emp.setFoto(rs.getBlob(6));
+				records.add(emp);
+				//employees.add(emp);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		return records;
+		//return employees;
+	}
+	
+public String chooseProducto(){
+		
+	    this.setInNombre(getProducto().getNombre());
+	    this.setInMarca(getProducto().getMarca());
+	    this.setInDireccion(getProducto().getDireccion());
+	    this.setInCalificacion(getProducto().getCalificacion());
+	    this.setInPrecio(getProducto().getPrecio());
+	    
+	    return null;
+	 }
+	
+	public String busquedaProducto() {
+		ResultSet rs = null;
+		PreparedStatement pst = null;
+		Connection con = ConeccionDB.getConnection();
+		String stm = "Select nombre, marca, direccion, calificacion, precio, foto from PRODUCTOSPRUEBA WHERE NOMBRE =?";
+		ArrayList<Producto> records = new ArrayList<Producto>();
+		//employees = new ArrayList<Employee>();
+		try {
+			pst = con.prepareStatement(stm);
+			pst.setString(1, getInNombre());
+			pst.execute();
+			rs = pst.getResultSet();
+			while (rs.next()) {
+				Producto emp = new Producto();
+				emp.setNombre(rs.getString(1));
+				emp.setMarca(rs.getString(2));
+				emp.setDireccion(rs.getString(3));
+				emp.setCalificacion(rs.getInt(4));
+				emp.setPrecio(rs.getString(5));
+				emp.setFoto(rs.getBlob(6));
+				records.add(emp);
+				//employees.add(emp);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+		return null;
+	}
+
+	public String createProducto() {
+
+		PreparedStatement pst = null;
+		Connection con = ConeccionDB.getConnection();
+		String stm = "INSERT INTO PRODUCTOS (nombre, marca, direccion, calificacion, precio) VALUES (?,?,?,?,?)";
+		try {
+			pst = con.prepareStatement(stm);
+			pst.setString(1, getInNombre());
+			pst.setString(2, getInMarca());
+			pst.setString(3, getInDireccion());
+			pst.setInt(4, getInCalificacion());
+			pst.setString(5, getInPrecio());
+			pst.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				pst.close();
+				con.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+
+		resetField();
+
+		return null;
+	}
+	
+private void resetField(){
+		
+		this.inNombre = "";
+		this.inMarca = "";
+		this.inDireccion = "";
+		this.inCalificacion = 0;
+		this.inPrecio = "";
+	}
+}
---
+++ ProyectoFinalJSF/src/com/tecsup/proyecto/bean/Producto.java
@@ -0,0 +1,81 @@
+package com.tecsup.proyecto.bean;
+
+import java.io.Serializable;
+import java.sql.Blob;
+
+
+
+public class Producto implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String nombre;
+	private String marca;
+	private String direccion;
+	private int calificacion;
+	private String precio;
+	private Blob foto;
+	
+	public Producto(String nombre, String marca, String direccion, int calificacion, String precio, Blob foto) {
+		super();
+		this.nombre = nombre;
+		this.marca = marca;
+		this.direccion = direccion;
+		this.calificacion = calificacion;
+		this.precio = precio;
+		this.foto = foto;
+	}
+
+	public Producto() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public int getCalificacion() {
+		return calificacion;
+	}
+
+	public void setCalificacion(int calificacion) {
+		this.calificacion = calificacion;
+	}
+	
+	public String getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(String precio) {
+		this.precio = precio;
+	}
+
+	public Blob getFoto() {
+		return foto;
+	}
+
+	public void setFoto(Blob foto) {
+		this.foto = foto;
+	}
+	
+	
+}
